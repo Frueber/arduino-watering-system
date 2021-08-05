01875416f5885126adbccdeed87f003f9fc2496c
Owen Shartle <oshartle@webstaurantstore.com>
2021-08-04 22:18:14 -0400

    Adding arduino program for watering a Pothos plant. - Using single Capacitive Soil Moisture Sensor. - Using single relay on 4 channel relay module. - Using single 5v water pump.

---
+++ CapacitiveSoilMoistureSensor_v1-2_Pothos/CapacitiveSoilMoistureSensor_v1-2_Pothos.ino
@@ -0,0 +1,58 @@
+// The Capacitive Soil Moisture Sensor v1.2 is active low.  
+// We define RELAY_ON and RELAY_OFF for clarity.  
+#define RELAY_ON LOW
+#define RELAY_OFF HIGH
+
+const int _airValue = 617;   // Replace the value with value when placed in air using calibration code.
+const int _waterValue = 349; // Replace the value with value when placed in water using calibration code.
+const int _waterPumpPin = 2;
+int soilMoistureSensorValue = 0;
+int soilMoisturePercent = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Ensure the digital pin for the water pump is off by default.
+  digitalWrite(_waterPumpPin, RELAY_OFF);
+  pinMode(_waterPumpPin, OUTPUT);
+}
+
+void loop() {
+  soilMoistureSensorValue = analogRead(A0);
+
+  Serial.print("Moisture Sensor Value: ");
+  Serial.println(soilMoistureSensorValue);
+  
+  soilMoisturePercent = map(
+    soilMoistureSensorValue,
+    _airValue,
+    _waterValue,
+    0,
+    100
+  );
+  
+  if (soilMoisturePercent > 100)
+  {
+    soilMoisturePercent = 100;
+  }
+  else if (soilMoisturePercent < 0)
+  {
+    soilMoisturePercent = 0;
+  }
+  
+  Serial.print("Soil Moisture: ");
+  Serial.print(soilMoisturePercent);
+  Serial.println("%");
+  
+  delay(250);
+
+  // Turn on/off water pump.
+  if(soilMoisturePercent <= 35)
+  {
+    digitalWrite(_waterPumpPin, RELAY_ON);
+  }
+  if(soilMoisturePercent >= 55)
+  {
+    digitalWrite(_waterPumpPin, RELAY_OFF);
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# arduino-watering-system
+# Arduino Watering System  
+
+
+
+---
+
+## Introduction  
+
+Trigger relays to power water pumps when soil moisture is low.  
+
+---
+
+## Hardware  
+
+### Microprocessor  
+
+- 1 Arduino Nano.  
+
+### Components  
+
+- 1 (* x) Capacitive Soil Moisture Sensor(s) v1.2.  
+- 1 Breadboard Power Supply Module 3.3V/5V.  
+- 1 4-Channel DC 5V Relay Module.  
+
+---
+
+### Wiring  
+
+A wiring diagram has been provided for each component of the full circuit.  
+These circuit diagrams are included in the [Schematics](Schematics) folder.  
+
+---
+
+## Contributors  
+
+Owen Shartle  
+
+---
+
+## Change Log  
+- 08/04/2020: Created.  
